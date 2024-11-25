ecafcaaf1be093796ba44e81d4e4eb9f9598d4ba
Jakub Łysiuk <242457@edu.p.lodz.pl>
2024-11-25 15:43:19 +0100

    some input handling experiments

--- DirectX_project/DirectX_project/WinMain.cpp
+++ DirectX_project/DirectX_project/WinMain.cpp
@@ -1,29 +1,98 @@
 #include <Windows.h>
+#include <string>
+#include <sstream>
+using namespace std;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+
+    case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+
+    case WM_KEYDOWN:
+        if (wParam == 'F')
+        {
+			SetWindowText(hWnd, "Respects");
+        }
+		break;
+
+    case WM_CHAR:
+        {
+		static std::string title;
+		title.push_back((char)wParam);
+		SetWindowText(hWnd, title.c_str());
+        }
+        break;
+
+    case WM_LBUTTONDOWN:
+        {
+		POINTS pt = MAKEPOINTS(lParam);
+        std::ostringstream oss;
+		oss << "(" << pt.x << "," << pt.y << ")";
+		SetWindowText(hWnd, oss.str().c_str());
+        }
+    }
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 
 int CALLBACK WinMain(
-	HINSTANCE hInstance, 
-	HINSTANCE hPrevInstance, 
-	LPSTR lpCmdLine, 
-	int nCmdShow)
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nCmdShow)
 {
-	const auto pClassName = L"DirectX11_JL";
-	// Register the window class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = NULL;
-	wc.hCursor = NULL;
-	wc.hbrBackground = NULL;
-	wc.lpszMenuName = NULL;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = NULL;
+    const auto pClassName = "DirectX11_JL";
+    // Register the window class
+    WNDCLASSEX wc = { 0 };
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = NULL;
+    wc.hCursor = NULL;
+    wc.hbrBackground = NULL;
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = pClassName;
+    wc.hIconSm = NULL;
 
-	RegisterClassEx(&wc);
+    RegisterClassEx(&wc);
 
-	//Create window instance
-	return 0;
+    //Create window instance
+    HWND hWnd = CreateWindowEx(
+        0,
+        pClassName,
+        "The Window",
+        WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+        200, 200, 640, 480,
+        NULL, NULL, hInstance, NULL
+    );
+    
+    //Show window
+    ShowWindow(hWnd, SW_SHOW);
+   
+    
+	//Message pump
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+    {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+    else
+    {
+		return msg.wParam;
+    }
+    
 }
